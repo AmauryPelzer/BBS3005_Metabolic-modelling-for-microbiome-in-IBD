067f8930eadab7ed93b108bf00beb8802aca0e7a
Amaury Pelzer <amaury.pelzer@gmail.com>
2022-06-09 13:29:26 +0200

    Adapting code for updated metagenomics

--- Scripts/Create_new_tsv.py
+++ Scripts/Create_new_tsv.py
@@ -1,15 +1,17 @@
 import re
 
 # open tsv file and read as a text string
-with open("ecs_relab.tsv", 'r') as f:
+with open(r".\Data\ecs_3.tsv", 'r') as f:
     my_csv_text = f.read()
 
 find_str = ':'
 find_str_2 = '\|'
 find_str_title = "# Gene Family"
+find_str_column_name = "_Abundance-CPM"
 
 replace_str = '\t'
 replace_str_title = "Gene Family\tName+Organism"
+replace_str_column_name = ""
 #replace_str_title = "# Gene Family\tName\tOrganism"
 
 # substitute
@@ -19,9 +21,11 @@ print("Replace colon: Done")
 #print("Replace vertical bar: Done")
 new_csv_str = re.sub(find_str_title, replace_str_title, new_csv_str)
 print("Replace title: Done")
+new_csv_str = re.sub(find_str_column_name, replace_str_column_name, new_csv_str)
+print("Replace column name: Done")
 
 # open new file and save
-new_csv_path = './ecs_relab_split.csv'
+new_csv_path = r".\Data\ecs_3_split.csv"
 with open(new_csv_path, 'w') as f:
     f.write(new_csv_str)
 print("Writing file: Done")
